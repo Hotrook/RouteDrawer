b662548bce2d578ad53d2568b2d1ef4947ed4556
Hotrook <mrozsebastian@wp.pl>
2017-08-08 20:50:45 +0200

    Initial Commit

---
+++ OpenGLLearn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLLearn
+//
+//  Created by Sebastian Mróz on 08.08.2017.
+//  Copyright © 2017 Sebastian Mróz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
